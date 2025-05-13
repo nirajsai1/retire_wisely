7a6a7508ce93dba9fd5c67c62a8811882e3aa748
niraj <neerajsai290@gmial.com>
2025-05-13 17:35:43 +0530

    Index.js updated

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ import ScrollToTop from './js/ScrollToTop';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div className='antialiased'>
     <BrowserRouter>
     <ScrollToTop />
     <App />
     </BrowserRouter>
+    </div>
   </React.StrictMode>
 );
 
